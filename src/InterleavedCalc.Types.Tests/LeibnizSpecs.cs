df72c36fefde8a123638870643f7ed7d2a8616a4
Aaron Stannard <aaron@petabridge.com>
2025-02-13 00:50:34 -0600

    validated Leibniz series

---
+++ src/InterleavedCalc.Types.Tests/LeibnizSpecs.cs
@@ -0,0 +1,15 @@
+﻿using InterleavedCalc.Types.Leibniz;
+
+namespace InterleavedCalc.Types.Tests;
+
+public class LeibnizSpecs
+{
+    [Fact]
+    public void ShouldCalculatePi()
+    {
+        var output = PiCalculator.CalculatePi(1000);
+        
+        // at 1000 iterations, we're at a pretty low precision for PI
+        Assert.True(output > 3.14);
+    }
+}
--- src/InterleavedCalc.Types.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InterleavedCalc.Types.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
